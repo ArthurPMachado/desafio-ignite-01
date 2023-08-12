9ab753b555aff9a70c5bd22034dcc33700fa6156
Arthur Machado <machadoparthur1@gmail.com>
2023-08-12 01:06:55 -0300

    create post and get routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,33 @@
+import { randomUUID } from "crypto";
 import { getRoutePath } from "./utils/getRoutePath.js";
+import { Database } from "./database.js";
+
+const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
     path: getRoutePath('/tasks'),
     handler: (request, response) => {
+      const tasks = database.select('tasks')
 
+      return response.end(JSON.stringify(tasks))
     }
   },
   {
     method: 'POST',
     path: getRoutePath('/tasks'),
     handler: (request, response) => {
+      request.body.completed_at = null
 
+      const task = {
+        id: randomUUID(),
+        ...request.body
+      }
+
+      database.insert('task', task)
+
+      return response.writeHead(201).end()
     }
   },
   {
